4ace851daf43db855465e8d35ab9b5708f997e35
Maik Schreiber <blizzy@blizzy.de>
2022-04-26 01:24:03 +0200

    eliminate init function by generating code

--- rangechecks.go
+++ rangechecks.go
@@ -1,5 +1,8 @@
 package consistent
 
+//go:generate go run ./cmd/rangeexprstyles/ rangechecks_styles.go
+//go:generate gofmt -w rangechecks_styles.go
+
 import (
 	"go/ast"
 	"go/token"
@@ -10,10 +13,8 @@ import (
 )
 
 const (
-	rangeChecksLeft    = "left"
-	rangeChecksCenter  = "center"
-	rangeChecksRight   = "right"
-	rangeChecksOutside = "outside"
+	rangeChecksLeft   = "left"
+	rangeChecksCenter = "center"
 )
 
 var rangeChecksFlagAllowedValues = []string{flagIgnore, rangeChecksLeft, rangeChecksCenter}
@@ -21,63 +22,6 @@ var rangeChecksFlagAllowedValues = []string{flagIgnore, rangeChecksLeft, rangeCh
 var rangeCheckFlagDesc = map[string]string{
 	rangeChecksLeft:   "write common term in range expression on the left",
 	rangeChecksCenter: "write common term in range expression in the center",
-}
-
-// bits: AND OR LEFT_LESS LEFT_GREATER RIGHT_LESS RIGHT_GREATER X_LEFT X_CENTER X_RIGHT X_OUTSIDE
-var rangeExprStyles = map[uint16]string{}
-
-func init() {
-	// x > low && x < high
-	rangeExprStyles[0b10_01_10_1000] = rangeChecksLeft
-
-	// x < low || x > high
-	rangeExprStyles[0b01_10_01_1000] = rangeChecksLeft
-
-	newStyles := map[uint16]string{}
-
-	for oldBits := range rangeExprStyles {
-		// build variants for center:
-		// x > low && x < high  ->  low < x && x < high
-		newBits := oldBits & 0b11_00_11_0000
-		newBits |= ^oldBits & 0b00_11_00_0000
-		newBits |= 0b00_00_00_0100
-		newStyles[newBits] = rangeChecksCenter
-
-		// build variants for right:
-		// x > low && x < high  ->  low < x && high > x
-		newBits = oldBits & 0b11_00_00_0000
-		newBits |= ^oldBits & 0b00_11_11_0000
-		newBits |= 0b00_00_00_0010
-		newStyles[newBits] = rangeChecksRight
-
-		// build variants for outside:
-		// x > low && x < high  ->  x > low && high > x
-		newBits = oldBits & 0b11_11_00_0000
-		newBits |= ^oldBits & 0b00_00_11_0000
-		newBits |= 0b00_00_00_0001
-		newStyles[newBits] = rangeChecksOutside
-	}
-
-	for k, v := range newStyles {
-		rangeExprStyles[k] = v
-	}
-
-	for k := range newStyles {
-		delete(newStyles, k)
-	}
-
-	// build variants where left and right are swapped:
-	// x > low && x < high  ->  x < high && x > low
-	for oldBits, oldStyle := range rangeExprStyles {
-		newBits := oldBits & 0b11_00_00_1111
-		newBits |= oldBits & 0b00_11_00_0000 >> 2
-		newBits |= oldBits & 0b00_00_11_0000 << 2
-		newStyles[newBits] = oldStyle
-	}
-
-	for k, v := range newStyles {
-		rangeExprStyles[k] = v
-	}
 }
 
 func checkRangeCheck(pass *analysis.Pass, expr *ast.BinaryExpr, mode string) {
